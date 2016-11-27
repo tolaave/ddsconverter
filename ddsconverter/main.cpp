6100fd39d853b207252ed9fd7b569ec6eaa02f2b
Toni Lääveri <toni.laaveri@fidisk.fi>
2016-11-27 20:48:55 +0200

    Initial Commit

---
+++ ddsconverter/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ddsconverter
+//
+//  Created by Toni Lääveri on 27.11.2016.
+//  Copyright © 2016 Toni Lääveri. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
